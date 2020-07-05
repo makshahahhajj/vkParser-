9fc445853de3e813953164aee5149da3188967e7
max <razin-04@bk.ru>
2020-07-05 22:50:39 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+def init_driver():
+    driver = webdriver.Chrome("C:/chromedriver.exe")
+    driver.get('https://vk.com')
+    input = driver.find_element_by_id('index_email')
+    input.send_keys('hello', Keys.ENTER)
+    time.sleep(10)
+    driver.quit()
+
+
+if __name__ == '__main__':
+     init_driver()
