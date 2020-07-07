8ae5079e3262635ec399192fca3529ffb422621e
max <razin-04@bk.ru>
2020-07-07 14:33:13 +0300

    add category

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
 
 def init_driver():
-    driver = webdriver.Chrome("C:/chromedriver.exe")
-    driver.get('https://vk.com')
-    input = driver.find_element_by_id('index_email')
-    input.send_keys('hello', Keys.ENTER)
-    time.sleep(10)
-    driver.quit()
+        driver = webdriver.Chrome("C:/chromedriver.exe")
+        driver.get('https://www.wildberries.ru/')
+        categories = driver.find_element_by_class_name('topmenus')
+        category = categories.find_elements_by_class_name('topmenus-item')
+
+        for li in category:
+            link_content = li.find_element_by_tag_name('a').get_attribute('textContent')
+            print(link_content)
+
+        input()
 
 
 if __name__ == '__main__':
      init_driver()
-fff
